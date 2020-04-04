a7be18210117ea1f52d4b667f2a3e8f20b671dc1
Daniele Fossemò <daniele.fossemo@outlook.it>
2020-04-04 20:19:43 +0200

    header, footer and head blade files

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,63 @@
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<title>Koparion – Book Shop HTML5 Template</title>
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Favicon -->
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('img/favicon.png') }}">
+
+<!-- Fonts -->
+<link rel="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800|Rufina:400,700" rel="stylesheet">
+
+<!-- all css here -->
+<!-- bootstrap v3.3.6 css -->
+<link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+<!-- animate css -->
+<link rel="stylesheet" href="{{ asset('css/animate.css') }}">
+<!-- meanmenu css -->
+<link rel="stylesheet" href="{{ asset('css/meanmenu.min.css') }}">
+<!-- owl.carousel css -->
+<link rel="stylesheet" href="{{ asset('css/owl.carousel.css') }}">
+<!-- font-awesome css -->
+<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+<!-- flexslider.css-->
+<link rel="stylesheet" href="{{ asset('css/flexslider.css') }}">
+<!-- chosen.min.css-->
+<link rel="stylesheet" href="{{ asset('css/chosen.min.css') }}">
+<!-- style css -->
+<link rel="stylesheet" href="{{ asset('style.css') }}">
+<!-- responsive css -->
+<link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
+<!-- modernizr css -->
+<script src="{{ asset('js/vendor/modernizr-2.8.3.min.js') }}"></script>
+
+<!-- all js here -->
+<!-- jquery latest version -->
+<script src="{{ asset('js/vendor/jquery-1.12.0.min.js') }}"></script>
+<!-- popper js -->
+<script src="{{ asset('js/popper.min.js') }}"></script>
+<!-- bootstrap js -->
+<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+<!-- owl.carousel js -->
+<script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+<!-- meanmenu js -->
+<script src="{{ asset('js/jquery.meanmenu.js') }}"></script>
+<!-- wow js -->
+<script src="{{ asset('js/wow.min.js') }}"></script>
+<!-- jquery.parallax-1.1.3.js -->
+<script src="{{ asset('js/jquery.parallax-1.1.3.js') }}"></script>
+<!-- jquery.countdown.min.js -->
+<script src="{{ asset('js/jquery.countdown.min.js') }}"></script>
+<!-- jquery.flexslider.js -->
+<script src="{{ asset('js/jquery.flexslider.js') }}"></script>
+<!-- chosen.jquery.min.js -->
+<script src="{{ asset('js/chosen.jquery.min.js') }}"></script>
+<!-- jquery.counterup.min.js -->
+<script src="{{ asset('js/jquery.counterup.min.js') }}"></script>
+<!-- waypoints.min.js -->
+<script src="{{ asset('js/waypoints.min.js') }}"></script>
+<!-- plugins js -->
+<script src="{{ asset('js/plugins.js') }}"></script>
+<!-- main js -->
+<script src="{{ asset('js/main.js') }}"></script>
