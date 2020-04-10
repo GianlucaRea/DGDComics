6c028164c3b8d5e7cdb5ec2c63924df2b887305b
Daniele Fossemò <daniele.fossemo@outlook.it>
2020-04-10 19:02:39 +0200

    some controller

--- app/Http/Controllers/ImageContoller.php
+++ app/Http/Controllers/ImageContoller.php
@@ -2,9 +2,119 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Image;
+use Illuminate\Support\Facades\DB;
+use Validator;
 
 class ImageContoller extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Image::get(),200);
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $rules=[
+            'comic_id' => 'required',
+            'image' => 'required'
+        ];
+        $validator = Validator::make($request->all(),$rules);
+        if($validator->fails()){
+            return response()->json($validator->errors(),400);
+        }
+
+        $image = Image::create($request->all());
+        return response()->json($image,201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $image = Image::find($id);
+        if(is_null($image)){
+            return response()->json(["message"=>'Record not found'],404);
+        }
+        return response()->json(Image::find($id),200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $image = Image::find($id);
+        if(is_null($image)){
+            return response()->json(["message"=>'Record not found'],404);
+        }
+        $image -> update($request -> all());
+        return response()->json($image,200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $image = Image::find($id);
+        if(is_null($image)){
+            return response()->json(["message"=>'Record not found'],404);
+        }
+        $image-> delete();
+        return response()->json(null,204);
+    }
+
+    public function getComic($id)
+    {
+        $image = Image::find($id)->comic()->get();
+
+        return response()->json($image, 200);
+    }
 }
