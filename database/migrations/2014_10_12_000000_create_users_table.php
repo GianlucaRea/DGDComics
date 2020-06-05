2d1946da0b83a749c76b018766cc4d4440cb0a25
GianlucaRea <reagianluca97@gmail.com>
2020-06-05 10:35:39 +0200

    partita iva

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -30,11 +30,11 @@ class CreateUsersTable extends Migration
         });
         DB::table('users')->insert([
 
-            ['name' => 'venditore' , 'surname' => 'scarso' , 'username' => 'VenditoreScarso' , 'age' => '22' , 'iva' => 'PARTITAIVA1111X' , 'phone_number' => '323456789' , 'email' => 'venditore.scarso@lalala.comma', 'password' => Hash::make('password2')],
-            ['name' => 'venditore' , 'surname' => 'professionista' , 'username' => 'VenditoreProfessionista' , 'age' => '22' , 'iva' => 'PARTITAIVA2222X' , 'phone_number' => '321654987' , 'email' => 'venditore.professionista@lalala.comma', 'password' => Hash::make('password3')],
-            ['name' => 'Daniele' , 'surname' => 'Fossemò' , 'username' => 'DanieleF198' , 'age' => '22' , 'iva' => 'PARTITAIVA0000X' , 'phone_number' => '123456789' , 'email' => 'daniele.fossemo@lalala.comma', 'password' => Hash::make('password')],
-            ['name' => 'Gianluca' , 'surname' => 'Rea' , 'username' => 'gianrea' , 'age' => '22' , 'iva' => 'PARTITAIVA0001X' , 'phone_number' => '123654321' , 'email' => 'gianluca.rea@dgdcomics.com', 'password' => Hash::make('password')],
-           // ['name' => 'Davide' , 'surname' => 'Ricci' , 'username' => 'Cronio' , 'age' => '21' , 'iva' => 'PARTITAIVA0000Y' , 'phone_number' => '3333333333' , 'email' => 'davide.ricci@fake.fke', 'password' => 'passuord'],
+            ['name' => 'venditore' , 'surname' => 'scarso' , 'username' => 'VenditoreScarso' , 'age' => '22' , 'partitaIva' => 'PARTITAIVA1111X' , 'phone_number' => '323456789' , 'email' => 'venditore.scarso@lalala.comma', 'password' => Hash::make('password2')],
+            ['name' => 'venditore' , 'surname' => 'professionista' , 'username' => 'VenditoreProfessionista' , 'age' => '22' , 'partitaIva' => 'PARTITAIVA2222X' , 'phone_number' => '321654987' , 'email' => 'venditore.professionista@lalala.comma', 'password' => Hash::make('password3')],
+            ['name' => 'Daniele' , 'surname' => 'Fossemò' , 'username' => 'DanieleF198' , 'age' => '22' , 'partitaIva' => 'PARTITAIVA0000X' , 'phone_number' => '123456789' , 'email' => 'daniele.fossemo@lalala.comma', 'password' => Hash::make('password')],
+            ['name' => 'Gianluca' , 'surname' => 'Rea' , 'username' => 'gianrea' , 'age' => '22' , 'partitaIva' => 'PARTITAIVA0001X' , 'phone_number' => '123654321' , 'email' => 'gianluca.rea@dgdcomics.com', 'password' => Hash::make('password')],
+           // ['name' => 'Davide' , 'surname' => 'Ricci' , 'username' => 'Cronio' , 'age' => '21' , 'partitaIva' => 'PARTITAIVA0000Y' , 'phone_number' => '3333333333' , 'email' => 'davide.ricci@fake.fke', 'password' => 'passuord'],
         ]);
 
         DB::table('users')->insert([
