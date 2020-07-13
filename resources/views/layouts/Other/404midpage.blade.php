15c37340c4a26bfd2699bb31babfdf79e3c20994
Daniele Fossemò <54411636+DanieleF198@users.noreply.github.com>
2020-07-13 15:33:16 +0200

    error 404 - google maps not working cause of api key

--- resources/views/layouts/Other/404midpage.blade.php
+++ resources/views/layouts/Other/404midpage.blade.php
@@ -1,16 +1,27 @@
+<style>
+    .DGDcomicsLink{
+        color: #f07c29;
+    }
+
+    .DGDcomicsLink:hover{
+        color: #ffb400;
+    }
+</style>
+
 <!-- section-element-area-start -->
-<div class="section-element-area ptb-70">
+<div class="section-element-area pb-70">
     <div class="container">
         <div class="row">
             <div class="col-lg-12">
                 <div class="entry-header text-center mb-20">
-                    <img src="img/3.jpg" alt="not-found-img" />
-                    </br>
-                    <p>Oops! Questa pagina non può essere trovata.</p>
+                    <img src="{{asset('img/immaginiNostre/error404.png')}}" alt="not-found-img" />
+                    <div class="mt-4"></div>
+                    <p><b>Oops! a quanto pare questa pagina non esiste!</b></p>
                 </div>
                 <div class="entry-content text-center mb-30">
-                    <p>Scusa, ma la pagina che stai cercando non è stata trovata. Perfavore, controlla se hai inserito il giusto URL.</p>
-                    <a href="{{ url('/') }}">Torna alla Home</a>
+                    <p>A quanto pare la pagina da lei cercata non è stata trovata o non esiste; la preghiamo di controllare se ha inserito correttamente l'URL.</p>
+                    <p>Se invece lei è arrivata in questa pagina a causa di un errore del nostro sito e l'errore persiste la preghiamo di <a href="{{ url('/contact') }}" class="DGDcomicsLink">contattarci</a>.</p>
+                    <a href="{{ url('/') }}" class="DGDcomicsLink">Torna alla Home</a>
                 </div>
             </div>
         </div>
